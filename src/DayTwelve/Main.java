9c3485ca3e96d9216f5e651a330ee70e045cf0db
Gmatieso <geoffrey.matieso@sare.africa>
2025-04-09 16:03:58 +0300

    Starting out with ArrayList

---
+++ src/DayTwelve/Main.java
@@ -0,0 +1,24 @@
+package DayTwelve;
+
+/*
+    * In Java a collection represent a group of objects. often referred to as elements.
+    * Collection provides a way to store, manipulate, and retrieve data. in a more organized manner.
+    * Java can always store objects
+    * Primary purpoe of collection is to store data. i.e object type of data.
+    * If we want to store primitive data. we use array .
+    * Collection can be Interfaces or Classes.
+    *
+    ! ArrayList
+    * ArrayList is a class that implements List interface
+    * Array can only store homogeneous (same type)
+    * ArrayList collection can store both homogeneous(same type) and heterogeneous(different data) . data
+    * ArrayList collection allow duplicate elements to be stored. i.e same object multiple times.
+    * Maintains Insertion Order. i.e The order in which elements are added will be the order in which they're retrieved.
+    * Indexing Support.
+    * Multiple Null Allowed.
+ */
+public class Main {
+    static public void main(String[] args) {
+
+    }
+}
