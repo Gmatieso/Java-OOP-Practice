8d8a6fcb5f4d40019b4598887cc70913e5910d5a
GeoffreyMatieso <matiesogeoffrey@gmail.com>
2025-06-11 16:17:54 +0300

    Code refactored

--- src/DaySixteen/Main.java
+++ src/DaySixteen/Main.java
@@ -2,35 +2,41 @@ package DaySixteen;
 
 public class Main {
     public static void main(String[] args){
-        MyThread myThread = new MyThread();
-        myThread.start();
+//        MyThread myThread = new MyThread();
+//        myThread.start();
+//
+//        MyRunnable myRunnable = new MyRunnable();
+//        Thread thread = new Thread(myRunnable);
+//        thread.start();
+//
+//        for(int count = 0; count < 5; count ++){
+//            System.out.println("In main thread, count is:" +  count);
+//        }
+//
+//        MyThread2 myThread2 = new MyThread2();
+//        myThread2.start();
+//
+//        Thread threadStateExample = new Thread(new ThreadStateExample());
+//
+//        System.out.println("Thread state:" + threadStateExample.getState()); // NEW
+//
+//        threadStateExample.start();
+//
+//        System.out.println("Thread state:" + threadStateExample.getState()); // RUNABLE
+//
+//        try {
+//            Thread.sleep(10000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//
+//        System.out.println("Thread state:" + threadStateExample.getState()); //TERMINATED
 
-        MyRunnable myRunnable = new MyRunnable();
-        Thread thread = new Thread(myRunnable);
-        thread.start();
-
-        for(int count = 0; count < 5; count ++){
-            System.out.println("In main thread, count is:" +  count);
-        }
-
-        MyThread2 myThread2 = new MyThread2();
-        myThread2.start();
-
-        Thread threadStateExample = new Thread(new ThreadStateExample());
-
-        System.out.println("Thread state:" + threadStateExample.getState()); // NEW
-
-        threadStateExample.start();
-
-        System.out.println("Thread state:" + threadStateExample.getState()); // RUNABLE
-
-        try {
-            Thread.sleep(10000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("Thread state:" + threadStateExample.getState()); //TERMINATED
-
-    }
+        Thread t0 = new Thread(new MyThreadPriority());
+        Thread t1 = new Thread(new MyThreadPriority());
+        t0.setPriority(Thread.MAX_PRIORITY);
+        t1.setPriority(Thread.MIN_PRIORITY);
+        t0.start();
+        t1.start();
+   }
 }
