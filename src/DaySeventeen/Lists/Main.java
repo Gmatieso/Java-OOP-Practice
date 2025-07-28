a72da8bebca873430ee27da4a92bcfad4ab6738a
Gmatieso <matiesogeoffrey@gmail.com>
2025-07-29 00:26:13 +0300

    Overview of List in Java

--- src/DaySeventeen/Lists/Main.java
+++ src/DaySeventeen/Lists/Main.java
@@ -10,10 +10,19 @@ public class Main {
         groceryLine.add("Beth");
         groceryLine.add("Sam");
 
+//        System.out.println(groceryLine);
+//        groceryLine.remove("Beth");
+//        System.out.println(groceryLine);
+//        groceryLine.remove(1);
+//        System.out.println(groceryLine);
+
         System.out.println(groceryLine);
-        groceryLine.remove("Beth");
-        System.out.println(groceryLine);
-        groceryLine.remove(1);
-        System.out.println(groceryLine);
+        int samIndex = groceryLine.indexOf("Sam");
+        System.out.println(samIndex);
+        System.out.println(groceryLine.size());
+
+        for (String name : groceryLine) {
+            System.out.println(name);
+        }
     }
 }
