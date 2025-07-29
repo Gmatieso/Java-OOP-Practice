0d49bef5b885f2e29df8642609e07750dc1c377c
Gmatieso <matiesogeoffrey@gmail.com>
2025-07-29 10:04:46 +0300

    Refactor: Adding Generics in Methods/Functions

--- src/DaySeventeen/Generics/Main.java
+++ src/DaySeventeen/Generics/Main.java
@@ -8,10 +8,15 @@ public class Main {
         Box<Milk> boxOfMilk = new Box<Milk>();
         Box<Oranges> boxOfOranges = new Box<Oranges>();
 
-        boxOfMilk.add(milk);
-        boxOfOranges.add(oranges);
+        debugAdd(milk,boxOfMilk);
+        debugAdd(oranges,boxOfOranges);
 
         boxOfMilk.remove().drink();
         boxOfOranges.remove().juggle();
+    }
+
+    static <T> void debugAdd(T item, Box<T> box){
+            System.out.println("Type:" + item.getClass().getSimpleName());
+            box.add(item);
     }
 }
